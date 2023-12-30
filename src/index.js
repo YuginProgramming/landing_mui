9e30b71e3664f9f9b912699b5d208ef603c33dd1
YuginProgramming <dudaryev@gmail.com>
2023-12-30 19:23:54 +0200

    updates1

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import Authentication from './pages/Authentication/Authentication';
 import MachineLearning from './pages/MachineLearning/MachineLearning';
 import Hosting from './pages/Hosting/Hosting';
 import Functions from './pages/Functions/Functions';
 import Database from './pages/Database/Database';
 import Storage from './pages/Storage/Storage';
-import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from '@mui/material/styles';
+import { dashboardTheme } from './dashboardTheme';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
-  <BrowserRouter>
+ReactDOM.render(
+  <ThemeProvider theme={dashboardTheme}>
+    <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="authentication" element={<Authentication />} />
@@ -26,4 +31,11 @@ root.render(
       </Route>
     </Routes>
   </BrowserRouter>
+  </ThemeProvider>,
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

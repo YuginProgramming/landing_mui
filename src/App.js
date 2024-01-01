521d0c10adb8d888f61e94cae26803020df793f9
YuginProgramming <dudaryev@gmail.com>
2024-01-01 11:24:45 +0200

    title correct

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import { Outlet } from "react-router-dom";
 import Grid from '@mui/material/Grid';
 import Header from './components/Header/Header';
 import { useLocation } from 'react-router-dom'; 
+import { mainNavbarItems } from './components/Navbar/const/navbarItems';
 
 function App() {
   const [title, setTitle] = useState(null);
   const location = useLocation();
   
   useEffect(() => {
-    const parsedTitle = location.pathname.replace(/\W/g, ' ');
+    const currentRoute = mainNavbarItems.find(item => {
+      const routeName = item.route.startsWith('/') ? item.route : `/${item.route}`;
+      return location.pathname.endsWith(routeName);
+    });
+    const parsedTitle = currentRoute ? currentRoute.label : 'Землекуп';
     setTitle(parsedTitle);
   }, [location]);
 
